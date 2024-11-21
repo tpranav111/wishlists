ee77d3d85c6deac957c3a30f4c5a828190251178
Pranav Thorat <thoratpranav@gmail.com>
2024-11-21 18:41:41 -0500

    search item and wl

--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -210,6 +210,7 @@ $(function () {
             table += '<th class="col-md-2">Updated Time</th>'
             table += '</tr></thead><tbody>'
             let firstWL = "";
+            
             for(let i = 0; i < res.length; i++) {
                 let WL = res[i];
                 table +=  `<tr id="row_${i}"><td>${WL.id}</td><td>${WL.name}</td><td>${WL.note}</td><td>${WL.is_favorite}</td><td>${WL.updated_time}</td><td>`;
@@ -314,6 +315,84 @@ $(function () {
 
         ajax.fail(function(res){
             clear_item_data()
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+    //list items
+    $("#item_search_btn").click(function () {
+
+        let I_name = $("#item_name").val();
+        let wish_id = $("#desired_item_wishlist").val();
+        //let available = $("#pet_available").val() == "true";
+
+        let queryString = ""
+
+        //if (I_name) {
+        queryString += 'search=' + I_name
+        flash_message(queryString)
+        //}
+        /*
+        if (category) {
+            if (queryString.length > 0) {
+                queryString += '&category=' + category
+            } else {
+                queryString += 'category=' + category
+            }
+        }
+        if (available) {
+            if (queryString.length > 0) {
+                queryString += '&available=' + available
+            } else {
+                queryString += 'available=' + available
+            }
+        }
+        */
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "GET",
+            //url: `/pets?${queryString}`,
+            url: `/wishlists/${wish_id}/items/name?${queryString}`,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax.done(function(res){
+            //alert(res.toSource())
+            $("#search_results_items").empty();
+            let table = '<table class="table table-striped" cellpadding="10">'
+            table += '<thead><tr>'
+            table += '<th class="col-md-2">ID</th>'
+            table += '<th class="col-md-2">Wishlist</th>'
+            table += '<th class="col-md-2">Category</th>'
+            table += '<th class="col-md-2">Favorite?</th>'
+            table += '<th class="col-md-2">Name</th>'
+            table += '<th class="col-md-2">Note</th>'
+            table += '<th class="col-md-2">Quantity</th>'
+            table += '<th class="col-md-2">Price</th>'
+            table += '</tr></thead><tbody>'
+            $("#wishlist_note").val(res.note);
+            update_item_data(res)
+            let firstItm = "";
+            //for(let i = 0; i < res.length; i++)
+            //let itm = res[i];
+                
+            table +=  `<tr id="row_${i}"><td>${res.id}</td><td>${res.wishlist_id}</td><td>${res.category}</td><td>${res.is_favorite}</td><td>${res.name}</td><td>${res.note}</td></tr>${res.quantity}</td><td>${res.price}</td></tr>`;
+
+            table += '</tbody></table>';
+            $("#search_results_items").append(table);
+
+            // copy the first result to the form
+            /*
+            if (firstPet != "") {
+                update_form_data(firstPet)
+            }
+            */
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
             flash_message(res.responseJSON.message)
         });
 
